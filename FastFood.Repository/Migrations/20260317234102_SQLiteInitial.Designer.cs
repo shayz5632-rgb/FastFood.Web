2cd987d45e02fdcbcd0d89d41d2f6e01a6ba37cb
Shayz <shayz5632@gmail.com>
2026-03-17 16:41:49 -0700

    Add SQLite migration

--- FastFood.Repository/Migrations/20260317234102_SQLiteInitial.Designer.cs
+++ FastFood.Repository/Migrations/20260317234102_SQLiteInitial.Designer.cs
@@ -4,6 +4,7 @@ using FastFood.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FastFood.Repository.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260317234102_SQLiteInitial")]
+    partial class SQLiteInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FastFood.Repository/Migrations/20260317234102_SQLiteInitial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastFood.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class SQLiteInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
